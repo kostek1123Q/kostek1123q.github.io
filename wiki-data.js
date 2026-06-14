e11cc43c0cf6e9670e1660e80720ef30a9f06fc7
kostek1123Q <pokef227@gmail.com>
2026-06-14 21:20:38 +0200

    Create wiki-data.js

---
+++ wiki-data.js
@@ -0,0 +1,37 @@
+const WIKI_PAGES = [
+    {
+        title: "Shibl",
+        file: "shibl.html",
+        category: "postacie"
+    },
+    {
+        title: "Gerhard Linkemeyer",
+        file: "gerhard-linkemeyer.html",
+        category: "postacie"
+    },
+    {
+        title: "Rozpustna Randy",
+        file: "rozpustna-randy.html",
+        category: "postacie"
+    },
+    {
+        title: "Josh Osbourne",
+        file: "josh-osbourne.html",
+        category: "postacie"
+    },
+    {
+        title: "BBW Jackie",
+        file: "bbw-jackie.html",
+        category: "postacie"
+    },
+    {
+        title: "Sklep",
+        file: "sklep.html",
+        category: "postacie"
+    },
+    {
+        title: "Ohuelizatorzy",
+        file: "ohuelizatorzy.html",
+        category: "muzyka"
+    }
+];
